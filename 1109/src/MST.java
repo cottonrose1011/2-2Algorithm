d00b31974f4cca1cf2ba49fe3d8d9c833d0dea94
cottonrose1011 <pjeka7er43@gmail.com>
2022-11-09 13:49:34 +0900

    1109 commit

---
+++ 1109/src/MST.java
@@ -0,0 +1,12 @@
+
+public class MST {
+	public int n=4, e=6;//125,133,416,247,324,345
+	
+	public void insert(int n, int e,int weight ,int[][] w) {
+		w[n][e] = weight;
+	}
+	
+	public void prim(int n, int w[][], int f) {
+		
+	}
+}
---
+++ 1109/src/MstDiver.java
@@ -0,0 +1,10 @@
+
+public class MstDiver {
+
+	public static void main(String[] args) {
+		MST mst = new MST();
+		//mst.insert(1, 2, 5, null);
+
+	}
+
+}
